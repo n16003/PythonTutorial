8638e14269d6c24e831f66210458a8666cd8c5d7
Uehara Reo <n16003@std.it-college.ac.jp>
2016-07-21 04:06:52 +0900

    じゃんけんファイルの追加

---
+++ janken/play.py
@@ -0,0 +1,75 @@
+HANDS = ('グー','チョキ','パー')
+
+
+def select_hand():
+    """
+    コンピューターの手をランダムに決める
+    :return:HAND'の中のいずれか。
+    """
+    import random
+
+    random.choice(HANDS)
+
+    return
+
+
+def judgement(player , computer):
+    """
+    じゃんけんの勝敗を判定する。
+    :param player:HANDSの中のどれか
+    :param computer:HANDSの中のどれか
+    :return:プレイヤーが勝ちの場合は１、あいこは０、負けは−１を返す
+    """
+    if player == computer:
+        if player == 1:
+        if player == 2:
+        if player == 3:
+        return 0
+    else:
+        if player == 1:
+            if computer == 2:
+                return 1
+            if computer == 3:
+                return -1
+        if player == 2:
+            if computer == 1:
+                return -1
+            if computer == 3:
+                return 1
+        if player == 3:
+            if computer == 1:
+                return 1
+            if computer == 2:
+                return -1
+
+
+def save_score(result):
+    """
+    'score.txt'に戦績を保存。
+    win:x lose:y draw:zのディクショナリデータを保存する。
+    :param result:
+    :return: None
+    """
+    f = open ("score.txt","w")
+
+    result = {'win' : x , 'lose' : y, 'draw' : z}
+
+    f.write(result)
+
+    f.close()
+
+    print >> f , "score.py"
+
+    return
+
+if __name__=='__main':
+    player = int (input('グー(1)/チョキ(2)/パー(3)を選んでください(数字):'))
+    computer = select_hand()
+    result =judgement(player , computer)
+    #コンピューターの手と勝敗の結果を表示
+
+    print(computer)
+
+    print(result)
+
+    save_score(result)
