7f0b461d695a21f98499143f5dea10b7434c64e7
Uehara Reo <n16003@std.it-college.ac.jp>
2016-07-21 22:48:24 +0900

    tempfile2

--- janken/play.py
+++ janken/play.py
@@ -1,4 +1,4 @@
-HANDS = ('グー','チョキ','パー')
+HANDS = ('グー', 'チョキ', 'パー')
 
 
 def select_hand():
@@ -8,69 +8,63 @@ def select_hand():
     """
     import random
 
-    random.choice(HANDS)
+    a = random.randint(0, 2)
 
-    return
+    return a
 
 
-def judgement(player , computer):
-    """
-    じゃんけんの勝敗を判定する。
-    :param player:HANDSの中のどれか
-    :param computer:HANDSの中のどれか
-    :return:プレイヤーが勝ちの場合は１、あいこは０、負けは−１を返す
-    """
+def judgement(player, computer):
     if player == computer:
-        if player == 1 or player == 2 or player == 3:
 
-            return 0
+        return 0
 
     else:
         if player == 1:
-            if computer == 2:
+            if computer == 1:
                 return 1
-            if computer == 3:
+            if computer == 2:
                 return -1
         if player == 2:
-            if computer == 1:
+            if computer == 0:
                 return -1
-            if computer == 3:
+            if computer == 2:
                 return 1
         if player == 3:
-            if computer == 1:
+            if computer == 0:
                 return 1
-            if computer == 2:
+            if computer == 1:
                 return -1
 
 
 def save_score(result):
-    """
-    'score.txt'に戦績を保存。
-    win:x lose:y draw:zのディクショナリデータを保存する。
-    :param result:
-    :return: None
-    """
-    f = open ("score.txt","w")
+    f = open("score.txt", "w")
+    x = y = z == 0
+    res = {'win': x, 'lose': y, 'draw': z}
 
-    pass
+    if judgement() == 0:
+        res['draw'] += 1
 
-    res = {'win' : x , 'lose' : y, 'draw' : z}
+    if judgement() == 1:
+        res['win'] += 1
+
+    if judgement() == -1:
+        res['lose'] += 1
 
     f.write(res)
 
     f.close()
 
-    print >> f , "score.py"
+    print>>f, "score.py"
 
     return
 
-if __name__=='__main__':
-    player = int (input('グー(1)/チョキ(2)/パー(3)を選んでください(数字):'))
+if __name__ == '__main__':
+    player = int(input('グー(1)/チョキ(2)/パー(3)を選んでください(数字):'))
     computer = select_hand()
-    result =judgement(player , computer)
-    #コンピューターの手と勝敗の結果を表示
+    result = judgement(player, computer)
+    # コンピューターの手と勝敗の結果を表示
 
-    print(computer)
+    print("computer:", HANDS[computer])
 
     print(result)
 
